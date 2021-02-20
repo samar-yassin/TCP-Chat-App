44568114eff532f6ec3be026cdd6cd5ec0543abe
samar yassin <samareeyassin155@gmail.com>
2021-02-20 04:36:42 +0200

    added send file and receive function

--- client.py
+++ client.py
@@ -1,19 +1,97 @@
-import socket
+#!/usr/bin/env python
+import socket,os,time
 
-host = socket.gethostname()
+host = 192.168.0.0		#change this with server ip
 port = 4444
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((host,port))
 
 
-message = input()
+
+def sendFile(fileData,client):
+	fileName =  os.path.basename(fileData)
+	filePath= os.path.abspath(fileName)
+	client.sendall(fileName.encode('ascii'))
+
+	f = open(fileName,'rb')
+	l = f.read(1024)
+	while (l):
+		client.send(l)
+		l = f.read(1024)
+	f.close()
+
+
+	print("done")
+
+
+
+def isFileTransfer(client_socket,text):
+	fileName = ""
+	found = text.find("send -f ")
+	if found != -1 :
+		fileName=text[8:]
+		fileName = fileName.strip()
+		isFile=os.path.isfile(fileName)
+		if isFile:
+			filePath= os.path.abspath(fileName)
+			return fileName
+		else :
+			print(f"$ WRONG FILE NAME : {fileName}")
+			return "notExist"
+	else :
+		return "notSending"
+
+
+def receiveFile():
+	fileData = client.recv(1024)
+	fileName = fileData.decode('ascii')
+
+	with open(fileName, 'wb') as f:
+		print ('file opened')
+		print("$ Receiving...")
+		while True:
+			try:
+				client.settimeout(5)
+				data =client.recv(1024)
+			except socket.timeout:
+				client.settimeout(None)
+				break
+
+			f.write(data)
+	print("file received")
+		
+
+
+
+
+
+
+message = input("$ ")
 
 while message != "exit" :
-	client.send(message.encode('ascii'))
+	transfer = isFileTransfer(client,message)
+	if(transfer=="notExist"):
+				message=input("$ ")
+				continue
+
+	elif transfer == "notSending" :
+				client.send(message.encode('ascii'))
+	else :
+				print('$ Sending...')
+				message="Sending a file"
+				client.send(message.encode('ascii'))	
+				time.sleep(3)		
+				sendFile(transfer,client);
+
 	data = client.recv(1024)
-	print ("$ Received : %s" %data.decode('ascii'))
-	message = input()
+	data = data.decode('ascii')
+	if(data == "Sending a file"):
+		receiveFile()
+	else:
+		print ("$ Server : %s" %data)
+
+	message = input("$ ")
 
 
 client.send(message.encode('ascii'))
